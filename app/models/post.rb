a7ab096f8825ef19834287ce2637d28a27a74d77
Liam Goguen <liamgoguen07@gmail.com>
2017-04-05 23:24:58 +0000

    finished w4d1 lesson

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,16 @@ class Post < ActiveRecord::Base
         if time_ago_in_minutes >= 60
             "#{(time_ago_in_minutes / 60).to_i} hours ago"
         else
-            "#{(time_ago_in_minutes.to_i} minutes ago"
+            "#{(time_ago_in_minutes.to_i)} minutes ago"
         end
     end
+    
+    def like_count
+        self.likes.size
+    end
+    
+    def comment_count
+        self.comments.size
+    end
+    
 end
