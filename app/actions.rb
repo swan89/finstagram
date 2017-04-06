12d1ef35c4741ef1850ec59ac099962559aec590
Liam Goguen <liamgoguen07@gmail.com>
2017-04-06 01:55:18 +0000

    added user signup page via forms

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,26 @@ get '/' do
      @posts = Post.order(created_at: :desc)
     erb (:index)
 end
+
+get '/signup' do         # if a user navigates to the path '/signup',
+     @user = User.new    # setup emtpy @user object
+     erb(:signup)        # render "app/views/signup.erb"
+end
+
+post '/signup' do
+     #grab user input values from params
+     email = params[:email]
+     avatar_url = params[:avatar_url]
+     username = params[:username]
+     password = params[:password]
+     
+     # instantiate a User
+     @user = User.new({email:email, avatar_url:avatar_url, username:username, password:password})
+     
+     if @user.save
+          "User #{username} saved!"
+     
+     else
+          erb(:signup)
+     end     
+end
